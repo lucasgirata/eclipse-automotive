7683b32b86829e6f70415435a18d9fc4416fac0c
Lucas C. Girata <125375176+lucasgirata@users.noreply.github.com>
2025-11-24 16:38:47 -0300

    Delete Carro.java

--- Carro.java
+++
@@ -1,8 +0,0 @@
-public class Carro extends Automovel {
-    protected String chassi;
-
-    public Carro(String marca, String cor, String modelo, int ano, double preco, String chassi) {
-        super(marca, cor, modelo, ano, preco);
-        this.chassi = chassi;
-    }
-}
